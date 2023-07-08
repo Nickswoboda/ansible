def3a6675185150d7417a75a2b0a6c6d5c5ca70e
nswob <nswob@email.com>
2023-07-08 21:57:03 +1200

    change run command to use http instead of ssh clone

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-pull -U git@github.com:Nickswoboda/ansible.git --vault-pass-file pass.txt -v ansible.yml
+ansible-pull -U https://github.com/Nickswoboda/ansible.git --vault-pass-file pass.txt -v ansible.yml
